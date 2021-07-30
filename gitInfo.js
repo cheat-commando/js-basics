818b8624899912834679125b0e265b2d187ad5fb
Carston Work <carston.work@gmail.com>
2021-07-30 10:08:28 -0600

    I completed the gitInfo assignment

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,9 @@
 */
 
 //CODE HERE
-
+const gitDefinition = `Git is a way of saving different versions of your coding projects at various stages of its development.
+It also allows you to create separate branches (not unlike alternate realities) of your project so that multiple people can work
+on the same project at the same time without interfering with each other.`
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +20,9 @@
 */
 
 //CODE HERE
-
+const gitHubDefinition = `GitHub is a website that stores remote repositories. First off, LOCAL repositories are stored on your machine's memory.
+REMOTE repositories are stored on GitHub's servers so that you and others can access them from any machine. I should also explain what 
+repositories are for. A repository contains the files needed for your project.`
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,7 +38,10 @@
 */
 
 //CODE HERE
-
+const init = {
+    code : "git init",
+    description : "This command activates the git program, and creates an invisible git file within the repository or folder you are currently browsing."
+}
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,6 +49,10 @@
 */
 
 //CODE HERE
+const clone = {
+    code : "git clone <insert GitHub URL here>",
+    description : "This command creates a copy of a repository from GitHub on your local machine."
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -50,7 +61,12 @@
 */
 
 //CODE HERE
-
+const status = {
+    code : "git status",
+    description : `This command reports of the current status of the repository you are current browsing; It will tell you if you have made any 
+    changes to the files that haven't been added, if you have added files, but haven't committed those changes yet, if it's linked to a remote repo, 
+    and if that remote repo is up-to-date on the changes you've made to the local repo.`
+}
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,7 +74,10 @@
 */
 
 //CODE HERE
-
+const add = {
+    code : "git add <file name>",
+    description : `This command stages a file. Staging a file means to prepare it to be committed. I see it as telling git to target this file.`
+}
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +85,11 @@
 */
 
 //CODE HERE
-
+const commit = {
+    code : "git commit -m 'type your commit message here'",
+    description : `This command takes the files you've added/staged and commits them as a new version of your project.
+    The easiest way to do this is with the little '-m' flag. If you don't, TERRIBLE THINGS WILL HAPPEN. Also, your commit message should be short, yet descriptive.`
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -75,3 +98,9 @@
 */
 
 //CODE HERE
+
+const push = {
+    code : "git push origin main",
+    description : `This command pushes your committed version of your files to a remote repo, which you should have already linked to
+    with the command "git remote add origin <GitHub URL>`
+}
