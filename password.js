ca7f2d83d4bb3589c9471aed48b733af6081ba54
Carston Work <carston.work@gmail.com>
2021-07-30 12:03:32 -0600

    My password validator now checks if the password is null, has any special characters, or starts with a capital, number, or underscore.

--- password.js
+++ password.js
@@ -5,16 +5,21 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-console.log("Welcome to the Password Validator Tool.");
+console.log("---Welcome to the Password Validator Tool---");
 
 const requestPassword = function() {
-    reader.question("Please enter you password: ",function(password){
-        if (password.length >= 10) {
-            console.log("Your password is long enough.");
-            reader.close();
-        } else {
-            console.log("Try again, sonny. Your password should be at least ten characters long.");
+    reader.question("Please enter your password: ", function(password){
+        let cleanPass = password.trim()
+        // startWithCapOrNum = (cleanPass[0] === cleanPass[0].toUpperCase); 
+        // lessthan10 = (cleanPass.length < 10);
+        // hasWeirdCharacterss = !!cleanPass.match(/\W/g);
+        // isNullOrUndefined = (!cleanPass);
+        if (!cleanPass || cleanPass[0] === cleanPass[0].toUpperCase() || !!cleanPass.match(/\W/g) || cleanPass.length < 10) {
+            console.log(`\n**ALARM NOISES**\n\nTry again! Here are some of our guidelines that I didn't bother to tell you before:\n\nYour password must have the following details:\n\n\t-at least 10 characters\n\t-cannot BEGIN with a capital letter, a number, or the underscore\n\t-can only include letters (a-z), numbers (0-9), and/or the underscore (_)\n`);
             requestPassword();
+        } else {
+            console.log('\nYour password is good enough, I suppose.');
+            reader.close();
         }
     });
 };
